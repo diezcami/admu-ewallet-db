2ccdcf5237b2ef7076d6d3cb924ad030681ebaf8
Cami Diez <diezcami@gmail.com>
2015-10-30 05:05:13 +0800

    Added manual JSON parsers

--- application/models/Stock_Model.php
+++ application/models/Stock_Model.php
@@ -1,15 +1,21 @@
 <?php 
     class Stock_Model extends CI_Model {
-
-        /**
-         * Error Descriptions:
-         * 200: Shop terminal has no stocks
-         */
-                
+        
         function __construct() {
             parent::__construct();
         }
-          
+
+        function sync_stocks ($shop_terminal_id, $item_id, $stock_ts) {
+            $data = array(
+                    'shop_terminal_id' => $shop_terminal_id,
+                    'item_id' => $item_id,
+                    'stock_ts' => $stock_ts
+                );
+            $this->db->insert('stock', $data);
+
+            return "Sync Successful";
+        }   
+
     }
 
 ?>
