91de92e96aa7cb2a2f2e1e4b96bb6ba4c736317d
Cami Diez <diezcami@gmail.com>
2015-10-30 05:05:13 +0800

    Added manual JSON parsers

--- application/controllers/Item_Order_Controller.php
+++ application/controllers/Item_Order_Controller.php
@@ -4,7 +4,29 @@ require APPPATH . '/libraries/REST_Controller.php';
 
 class Item_Order_Controller extends REST_Controller {
 
+   /**
+    * This method receives a JSON from the Android App containing Item Orders
+    *
+    * @post_params: none
+    * @output: Outputs decoded JSON array to Model
+    */
+   function sync_post() {
+        $params = $this->input->post('params');
+        $params = substr ($params, 1, -1);
+        $parsable = explode ('},', $params);
 
+        $this->load->model('Item_Order_Model');
+
+        foreach($parsable as $item) {
+            $buy_transaction_id = substr ($item, 22, 2);
+            $item_id = substr ($item, 35, 3);
+            $quantity = substr ($item, 50, 1);
+            $this->Item_Order_Model->sync_items($buy_transaction_id, $item_id, $quantity);
+        }
+
+        $this->response($parsable, 404);
+   }
 }
-
-
+/*
+[{"buy_transaction_id":01,"item_id":001,"quantity":3},{"buy_transaction_id":02,"item_id":003,"quantity":6}]
+*/
