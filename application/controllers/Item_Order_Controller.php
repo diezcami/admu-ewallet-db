ced59dae002846975031b95a25d13ec4945d6793
Cami Diez <diezcami@gmail.com>
2015-11-01 19:35:36 +0800

    Added documentation, quantity parsing functions

--- application/controllers/Item_Order_Controller.php
+++ application/controllers/Item_Order_Controller.php
@@ -6,7 +6,14 @@ class Item_Order_Controller extends REST_Controller {
 
    /**
     * This method receives a JSON from the Android App containing Item Orders
+    * Sample JSON (11/1, NOT FINAL):
+    * [{"buy_transaction_id":01,"item_id":001,"quantity":3}]
     *
+    * TODO:
+    *  Fix Buy_Transaction_ID Formatting (2->5)
+    *  Quantity Parsing
+    *
+    * @flow: App->Server
     * @post_params: none
     * @output: Outputs decoded JSON array to Model
     */
@@ -14,9 +21,9 @@ class Item_Order_Controller extends REST_Controller {
         $params = $this->input->post('params');
         $params = substr ($params, 1, -1);
         
-        if( strpos($params, "},")  || strpos($params, "},")==0 ){
+        if ( strpos($params, "},")  || strpos($params, "},")==0 ){
             $parsable = explode ('},', $params);
-        }else{
+        } else {
             $parsable = array($params);
         }
 
@@ -25,13 +32,27 @@ class Item_Order_Controller extends REST_Controller {
         foreach($parsable as $item) {
             $buy_transaction_id = substr ($item, 22, 2);
             $item_id = substr ($item, 35, 3);
-            $quantity = substr ($item, 50, 1);
+            $quantity = quantity_parse(substr ($item, 50);
             $this->Item_Order_Model->sync_items($buy_transaction_id, $item_id, $quantity);
         }
 
         $this->response($parsable, 404);
    }
+
+   /**
+    * Determines the quantity passed in a JSON string.
+    * Assumptions include the quantity being passed last, though it's easily tweakable if otherwise required.
+    * 
+    * @param  [string] A substring of the received JSON input 
+    * @return [int] The actual quantity perscribed in the given JSON string
+    */
+   function quantity_parse ($json_string) {
+        $quantity = 0;
+        while (strlen($json_string)>0 && $json_string != '}') {
+            $quantity += $json_string[0];
+            $json_string = substr ($json_string, 1);
+        }
+
+        return $quantity
+   }
 }
-/*
-[{"buy_transaction_id":01,"item_id":001,"quantity":3},{"buy_transaction_id":02,"item_id":003,"quantity":6}]
-*/
