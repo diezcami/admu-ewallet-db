df5e453efe434c007a5e4acb300775176a106665
Cami Diez <diezcami@gmail.com>
2015-11-15 15:36:16 +0800

    zz

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -1,17 +1,16 @@
 <?php
 class Users extends CI_Controller {
-  public function __construct()
-  {
-    parent::__construct();
-    $this->load->model('User_model');
-  }
-  public function index()
-  {
-    $data['users'] = $this->User_model->get_users();
-    var_dump($data);
-  }
-  public function view()
-  {
-    $data['users'] = $this->User_model->get_users();
-  }
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('User_Model');
+    }
+
+    public function index() {
+        $data['users'] = $this->User_Model->get_users();
+        var_dump($data);
+    }
+
+    public function view() {
+        $data['users'] = $this->User_Model->get_users();
+    }
 }
--- application/models/User_Model.php
+++ application/models/User_Model.php
@@ -1,5 +1,5 @@
 <?php 
-    class User_model extends CI_Model {
+    class User_Model extends CI_Model {
 
         /**
          * Error Descriptions:
