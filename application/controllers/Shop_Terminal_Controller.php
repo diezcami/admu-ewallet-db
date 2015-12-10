d9d108178fb99f7f5d1fc1fed1e188ecee6ab810
Cami Diez <diezcami@gmail.com>
2015-12-10 23:49:26 +0800

    Added BT balance jsons

--- application/controllers/Shop_Terminal_Controller.php
+++ application/controllers/Shop_Terminal_Controller.php
@@ -20,4 +20,22 @@ class Shop_Terminal_Controller extends REST_Controller {
         $data = $this->Shop_Terminal_Model->get_shop_terminal_pin($shop_terminal_id);
         $this->response($data, 404);
    }
+
+   /**
+    * This method returns or updates the balance of a user
+    *
+    * @post_params: id_number, [new_balance]
+    * @output: JSON of user's balance
+    */
+   function balance_post() {
+        $shop_terminal_id = $this->post('shop_terminal_id');
+        $new_balance = $this->post('new_balance');
+        $this->load->model('Shop_Terminal_Model');
+        if ($new_balance !== NULL) {
+          $data = $this->Shop_Terminal_Model->set_balance($shop_terminal_id, $new_balance);
+        } else {
+          $data = $this->Shop_Terminal_Model->get_balance($shop_terminal_id);
+        }
+        $this->response($data, 404);
+   }
 }
--- application/models/Shop_Terminal_Model.php
+++ application/models/Shop_Terminal_Model.php
@@ -11,6 +11,30 @@
             parent::__construct();
         }
 
+        function get_balance($shop_terminal_id) {
+            $query = $this->db->query("SELECT balance FROM shop_terminal WHERE shop_terminal_id = '{$shop_terminal_id}'");
+            if($query->num_rows() > 0) {
+                $ret = $query->row();
+            } else {
+                $ret = '100';
+            }
+
+            return $ret;
+        }
+
+        function set_balance($shop_terminal_id, $new_balance) {
+            $new_balance = floatval($new_balance);
+            if ($shop_terminal_id>0 && $new_balance>0) {
+                $query = $this->db->query("UPDATE shop_terminal SET balance='{$new_balance}' WHERE shop_terminal_id = '{$shop_terminal_id}'");
+                $ret = $this->db->query("SELECT balance FROM shop_terminal WHERE shop_terminal_id = '{$shop_terminal_id}'")->row();
+            } else {
+                $ret = '100';
+            }
+
+            return $ret;
+        }
+
+
         function get_shop_terminals(){
             $query = $this->db->query ("SELECT * FROM shop_terminal");
             if($query->num_rows() > 0) {
